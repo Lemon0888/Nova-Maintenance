f6dbb679bcfe5947d8b6be2fa9e37193fb70004d
Lemon0888 <lemonlime0888@gmail.com>
2025-05-30 19:16:46 +0100

    Improved sfc functionality - requires debugging and testing
    Changes to be committed:
    modified:   Endpoint/main.py

--- Endpoint/main.py
+++ Endpoint/main.py
@@ -18,26 +18,28 @@ def run_command(command):
     return result.stdout, result.stderr, result.returncode
 #------------------------------------------------------------------------
 def sfc():
+    global sfc_success, sfc_code, returncode, elevation_required, sfc_output, sfc_error
     stdout, stderr, returncode = run_command("sfc /scannow")
-    global sfc_success, sfc_code
-    if returncode == 0:
-        sfc_success = "SFC scan completed successfully."
-        return sfc_success, stdout, stderr
+    output = stdout.decode()
+    output_stripped = output.strip()
+    print(output_stripped)
+    #print(stderr.decode())
+    if "Windows Resource Protection did not find any integrity violations" in output_stripped:
+        sfc_code = 0
+    elif "Windows Resource Protection found corrupt files and successfully repaired them" in output_stripped:
+        sfc_code = 1
+    elif "Windows Resource Protection found corrupt files but was unable to fix some of them" in output_stripped:
+        sfc_code = 2
+    elif "Windows Resource Protection could not perform the requested operation" in output_stripped:
+        sfc_code = 3
+    elif "Windows Resource Protection could not start the repair service" in output_stripped:
+        sfc_code = 4
+    elif "You must be an administrator" in output_stripped:
+        elevation_required = True
+        sfc_code = 5
     else:
         sfc_success = False
-        if "Windows Resource Protection found corrupt files and successfully repaired them" in stderr.decode():
-            sfc_code = 1
-        elif "Windows Resource Protection did not find any integrity violations" in stderr.decode():
-            sfc_code = 0
-        elif "Windows Resource Protection found corrupt files but was unable to fix some of them" in stderr.decode():
-            sfc_code = 2
-        elif "Windows Resource Protection could not perform the requested operation" in stderr.decode():
-            sfc_code = 3
-        elif "Windows Resource Protection could not start the repair service" in stderr.decode():
-            sfc_code = 4
-        else:
-            sfc_success = False
-            sfc_code = -1
+        sfc_code = -1
 
 sfc()
 print(sfc_success, sfc_code)
