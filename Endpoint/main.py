eef9d3186f2d82c6ad4fb68063756383848a232d
Lemon0888 <lemonlime0888@gmail.com>
2025-06-01 21:29:14 +0100

    Attempts to fix SFC issues - overall unsuccessful. Changes to be committed: modified:   Endpoint/main.py

--- Endpoint/main.py
+++ Endpoint/main.py
@@ -20,21 +20,21 @@ def run_command(command):
 def sfc():
     global sfc_success, sfc_code, returncode, elevation_required, sfc_output, sfc_error
     stdout, stderr, returncode = run_command("sfc /scannow")
-    output = stdout.decode()
-    output_stripped = output.strip()
+    output = stdout.decode('utf-8')
+    print(output)
+    output_stripped = output.strip().lower()
     print(output_stripped)
-    #print(stderr.decode())
-    if "Windows Resource Protection did not find any integrity violations" in output_stripped:
+    if "windows resource protection did not find any integrity violations" in output_stripped:
         sfc_code = 0
-    elif "Windows Resource Protection found corrupt files and successfully repaired them" in output_stripped:
+    elif "windows resource protection found corrupt files and successfully repaired them" in output_stripped:
         sfc_code = 1
-    elif "Windows Resource Protection found corrupt files but was unable to fix some of them" in output_stripped:
+    elif "windows resource protection found corrupt files but was unable to fix some of them" in output_stripped:
         sfc_code = 2
-    elif "Windows Resource Protection could not perform the requested operation" in output_stripped:
+    elif "windows resource protection could not perform the requested operation" in output_stripped:
         sfc_code = 3
-    elif "Windows Resource Protection could not start the repair service" in output_stripped:
+    elif "windows resource protection could not start the repair service" in output_stripped:
         sfc_code = 4
-    elif output_stripped.contains("You must be an administrator"):
+    elif "you must be an administrator" in output_stripped:
         elevation_required = True
         sfc_code = 5
     else:
